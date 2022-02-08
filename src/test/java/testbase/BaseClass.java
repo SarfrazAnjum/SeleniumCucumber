59c9d7524e8dd387b85599991fcd4f37b6886e0f
SarfrazAnjum <sir4ozz@gmail.com>
2022-02-08 01:25:46 -0500

    add chromeoptions

--- src/test/java/testbase/BaseClass.java
+++ src/test/java/testbase/BaseClass.java
@@ -3,6 +3,7 @@ package testbase;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -12,7 +13,12 @@ public class BaseClass{
     public  static WebDriver start() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         //System.setProperty("webdriver.chrome.driver", "src/test/resources/features/drivers/chromedriver");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("disable-infobars");
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+
+        driver = new ChromeDriver(options);
         driver.get("https://www.saucedemo.com/");
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
